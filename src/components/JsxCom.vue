02bc1b919d8e9e2e61e054a9f2c7ebfc0a6c81c3
fangchaoqun <fcqun@sina.cn>
2021-05-14 18:09:20 +0800

    feat: jsx配置

---
+++ src/components/JsxCom.vue
@@ -0,0 +1,17 @@
+<script lang="jsx" setup>
+import { reactive, defineComponent } from "vue";
+
+const state = reactive({ num: 0 });
+
+// https://github.com/vitejs/vite/tree/main/packages/plugin-vue-jsx
+export default defineComponent({
+  render() {
+    return (
+      <div>
+        num : {state.num}
+        <button onClick={() => state.num++}>btn</button>
+      </div>
+    );
+  },
+});
+</script>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import legacy from "@vitejs/plugin-legacy";
+import VueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    VueJsx(),
     // https://github.com/vitejs/vite/tree/main/packages/plugin-legacy
     legacy({
       // targets: ["defaults", "not IE 11"],
