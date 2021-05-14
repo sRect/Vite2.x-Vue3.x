ab586e20e7db6e575ae4566e1b786ee95da5f011
fangchaoqun <fcqun@sina.cn>
2021-05-14 15:40:41 +0800

    feat: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button @click="state.count++">count is: {{ state.count }}</button>
+  <p class="p">name: <span>{{jsonData.name}}</span>, age: {{jsonData.age}}</p>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue';
+import jsonData from '@/assets/test.json';
+import './helloWorld.css';
+
+defineProps({
+  msg: {
+    type: String,
+    default: 'hello'
+  }
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped lang="less">
+@import './test.less';
+a {
+  color: #42b983;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from "@vitejs/plugin-legacy";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [
+    vue(),
+    // https://github.com/vitejs/vite/tree/main/packages/plugin-legacy
+    legacy({
+      // targets: ["defaults", "not IE 11"],
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+  server: {
+    port: 8080,
+  },
+});
