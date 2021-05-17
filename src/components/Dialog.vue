446219eecf418e4cd2c4c9c960e1b2f0dfa25833
fangchaoqun <fcqun@sina.cn>
2021-05-17 18:09:02 +0800

    feat: teleport

---
+++ src/components/Dialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <teleport to="body">
+    <div class="dialog">
+      <div class="dialog_wrapper">
+        <div class="dialog_header" v-if="title">
+          <slot name="header">
+            <span>{{ title }}</span>
+          </slot>
+        </div>
+      </div>
+      <div class="dialog_content">
+        <slot></slot>
+      </div>
+      <div class="dialog_footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    default: 'title title'
+  }
+})
+</script>
+
+<style lang="less" scope>
+  .dialog {
+    width: 200px;
+    height: 300px;
+    background-color: #fff;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  }
+</style>
--- src/components/Hooks.vue
+++ src/components/Hooks.vue
@@ -5,11 +5,27 @@
     <p>doubleCount: {{ doubleCount }}</p>
     <button @click="increase(4)">increase</button>
     <button @click="decrease(2)">increase</button>
+    
+    <div>
+      <Dialog v-if="state.visible" />
+
+      <button @click="handleDialogVisible">dialog</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import {reactive} from 'vue';
 import useCount from "@/hooks/useCount";
+import Dialog from "@/components/Dialog.vue";
+
+const state = reactive({
+  visible: false
+});
 
 const { count, doubleCount, increase, decrease } = useCount(10);
+
+function handleDialogVisible() {
+  state.visible = !state.visible;
+}
 </script>
